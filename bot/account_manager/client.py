1059de76da25d22d0b516e2d44e205062e45d5de
Aidar P <aidar.pochanov@gmail.com>
2022-05-14 01:26:37 +0100

    adding account_manager with docker files

---
+++ bot/account_manager/client.py
@@ -0,0 +1,118 @@
+import requests
+import boto3
+import logging
+import schedule
+import time
+from http import HTTPStatus
+
+
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.NOTSET)
+logging.basicConfig(level=logging.INFO)
+
+
+URL = 'https://api.thegraph.com/subgraphs/name/voltzprotocol/v1-2'
+BASE_QUERY = """{{
+    positions(first: {count}, skip: {offset}) {{
+        id
+        tickLower {{
+          value
+        }}
+        tickUpper {{
+          value
+        }}
+        margin
+        owner {{
+          id
+        }}
+      }}
+    }}"""
+
+
+def fetch_and_write_positions(url: str, table: boto3.resource):
+  count: int = 1000
+  offset: int = 0
+  while True:
+    positions = fetch_positions(url, count, offset)
+    if not positions:
+      break
+    write_positions(positions, table)
+    offset += count
+
+
+def fetch_positions(url: str, count: int, offset: int):
+  """
+  Method to fetch voltz positions from the graph api
+  """
+  resp = requests.post(url, json={'query': BASE_QUERY.format(count=count, offset=offset)})
+  
+  if resp.status_code == HTTPStatus.OK:
+    positions = resp.json()['data']['positions'] 
+    logger.info(f"fetched {count} positions with {offset} offset")
+  else:
+    raise RuntimeError(status_code=resp.status_code)
+
+  return positions
+
+
+def write_positions(positions: dict, table: boto3.resource):
+  """
+  Method to write positions to dynamodb
+  """
+  try:
+    with table.batch_writer() as batch:
+      for position in positions:
+        batch.put_item(position)
+    logger.info("wrote positions to db")
+  except Exception as e:
+    logger.exception(e)
+
+
+
+def get_or_create_table() -> boto3.resource:
+  """
+  Method to get or create dynamodb table
+  """
+  dynamodb = boto3.resource(
+    'dynamodb',
+    endpoint_url='http://localhost:4566' # get rid of this when putting table on AWS
+  )
+  table = dynamodb.Table('Positions')
+  try:
+    table.table_status
+    logger.info("table 'Positions' already exists")
+  except dynamodb.meta.client.exceptions.ResourceNotFoundException:
+    dynamodb.create_table(
+      AttributeDefinitions=[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'S'
+        },
+    ],
+    TableName='Positions',
+    KeySchema=[
+        {
+            'AttributeName': 'id',
+            'KeyType': 'HASH'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 123,
+        'WriteCapacityUnits': 123
+    },)
+    logger.info("created 'Positions' table")
+  return table
+
+
+def run_client():
+  table = get_or_create_table()
+  fetch_and_write_positions(URL, table)
+
+
+if __name__ == '__main__':
+  # schedule.every(2).minutes.do(run_client)
+
+  # while True:
+  #   schedule.run_pending()
+  #   time.sleep(1)
+  run_client()
--- bot/scripts/liquidation.py
+++
@@ -1,5 +0,0 @@
-from brownie import network, Contract
-
-def main():
-    contract = Contract.from_explorer("0x15e3484EB4Ae66B9186699DB76024cBC363c1f2B")
-    print("received contract")
